fa69ddca2fde6621c1eebe1f0819b499b6d28a7f
yongdeng <yongdeng@xiaoman.cn>
2024-01-25 13:35:13 +0800

    添加模块依赖

---
+++ hihcxcn-server/src/main/java/cn/hihcxcn/patek/philippe/Main.java
@@ -0,0 +1,10 @@
+package cn.hihcxcn.patek.philippe;
+
+/**
+ * @author yongdeng on 24/01/2024.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ hihcxcn-start/src/main/java/cn/hihcxcn/patek/philippe/Application.java
@@ -0,0 +1,17 @@
+package cn.hihcxcn.patek.philippe;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yongdeng on 24/01/2024.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
